eb4a4780eb9e05c66cafd128555fab2240b78e5d
BREWIG615 <craig.615@protonmail.com>
2023-06-05 09:12:49 -0400

    initial commit

---
+++ point.py
@@ -0,0 +1,32 @@
+import jinja2
+import os 
+from jinja2 import Template
+import pandas as pd 
+import re
+
+latex_knowledgd_env = jinja2.Environment(
+    block_start_string = '\BLOCK{',
+    block_end_string = '}',
+    variable_start_string = '\VAR{',
+    variable_end_string = '}',
+    comment_start_string = '\#{',
+    comment_end_string = '}',
+    line_statement_prefix = '%%',
+    line_comment_prefix = '%#',
+    trim_blocks = True,
+    autoescape = False,
+    loader = jinja2.FileSystemLoader(os.path.abspath('.'))
+)
+
+template = latex_knowledgd_env.get_template('point.jinja2')
+
+if __name__ == "__main__":
+
+    df = pd.read_excel('point.xlsx')
+
+    rows = df.to_dict(orient = 'records')
+
+    with open("point.tex", "w") as f:
+        f.write(template.render(
+            rows = rows
+            ))
